99a064d884416ed088b79ac6f451761d577c2544
Akshita Mittal <94302233+drnkyda@users.noreply.github.com>
2024-08-29 17:24:51 +0530

    Update core.py with diff no. of columns for data
    Different number of columns for core data files, added if statement to load both kinds. --Akshita, PSU

--- PyART/catalogs/core.py
+++ PyART/catalogs/core.py
@@ -48,40 +48,113 @@ class CoRe(Waveform):
             self.read_h_h5(basepath)
         else:
             raise NameError('kind not recognized')
-        
+
     def read_h_txt(self, basepath):
-        # find all modes under basepath
-        modes = glob.glob(basepath+'/Rh_l*.txt')
+        # find all files under basepath
+        modes = glob.glob(basepath + '/Rh_l*.txt')
 
         r = []
-        # find extraction radii
+        # Extract extraction radii from filenames
         for m in modes:
             r.append(m.split('/')[-1].split('_')[-1].split('.')[0])
-        # find largest extraction radius
+        
+        # largest extraction radius
         r = list(set(r))
         imaxr = np.argmax([int(rr[1:]) for rr in r])
+        largest_radius = r[imaxr]
+
+        # Dictionary to store data
+        d = {}
 
         # read modes
-        d = {}
         for m in modes:
-            
             this_r = m.split('/')[-1].split('_')[-1].split('.')[0]
-            if this_r != r[imaxr]: continue
             
+            if this_r != largest_radius:
+                continue
+                
             ell = int(m.split('/')[-1].split('_')[1][1:])
             emm = int(m.split('/')[-1].split('_')[2][1:].split('.')[0])
-            if self.ell_emms != 'all':
-                if (ell, emm) not in self.ell_emms: continue
-            d[(ell, emm)] = {}
 
-            # u/M:0 Reh/M:1 Imh/M:2 Momega:3 A/M:4 phi:5 t:6
-            u, re, im, Momg, A, phi, t = np.loadtxt(m, unpack=True, skiprows=3)
-            d[(ell, emm)] ={
-                'A': A,              'p': phi,
-                't': t,              'real': re,             'imag': im
-            }
-        self._t = t
-        self._u = u
-        self._hlm = d
-        pass
+            if self.ell_emms != 'all':
+                if (ell, emm) not in self.ell_emms:
+                    continue
+
+            # determine number of columns 
+            with open(m, 'r') as f:
+                for _ in range(3):  # Skip header rows
+                    next(f)
+                first_data_line = f.readline().strip()
+            num_columns = len(first_data_line.split())
+
+            # load data based on number of columns
+            if num_columns == 7:
+                u, re, im, Momg, A, phi, t = np.loadtxt(m, unpack=True, skiprows=3)
+                d[(ell, emm)] = {
+                    'A': A,
+                    'p': phi,
+                    't': t,
+                    'real': re,
+                    'imag': im
+                }
+            elif num_columns == 9:
+                u, re, im, redh, imdh, Momg, A, phi, t = np.loadtxt(m, unpack=True, skiprows=3)
+                d[(ell, emm)] = {
+                    'A': A,
+                    'p': phi,
+                    't': t,
+                    'real': re,
+                    'imag': im,
+                    'redh': redh,
+                    'imdh': imdh
+                }
+            else:
+                raise ValueError(f"Unexpected number of columns ({num_columns}) in file {m}")
+
+        # check if d empty and update
+        if d:
+            self._t = t
+            self._u = u
+            self._hlm = d
+            self._A = A
+            self._re = re
+            self._im = im
+
+            pass
+        
+    # def read_h_txt(self, basepath):
+    #     # find all modes under basepath
+    #     modes = glob.glob(basepath+'/Rh_l*.txt')
+
+    #     r = []
+    #     # find extraction radii
+    #     for m in modes:
+    #         r.append(m.split('/')[-1].split('_')[-1].split('.')[0])
+    #     # find largest extraction radius
+    #     r = list(set(r))
+    #     imaxr = np.argmax([int(rr[1:]) for rr in r])
+
+    #     # read modes
+    #     d = {}
+    #     for m in modes:
+            
+    #         this_r = m.split('/')[-1].split('_')[-1].split('.')[0]
+    #         if this_r != r[imaxr]: continue
+            
+    #         ell = int(m.split('/')[-1].split('_')[1][1:])
+    #         emm = int(m.split('/')[-1].split('_')[2][1:].split('.')[0])
+    #         if self.ell_emms != 'all':
+    #             if (ell, emm) not in self.ell_emms: continue
+    #         d[(ell, emm)] = {}
+
+    #         # u/M:0 Reh/M:1 Imh/M:2 Momega:3 A/M:4 phi:5 t:6
+    #         u, re, im, Momg, A, phi, t = np.loadtxt(m, unpack=True, skiprows=3)
+    #         d[(ell, emm)] ={
+    #             'A': A,              'p': phi,
+    #             't': t,              'real': re,             'imag': im
+    #         }
+    #     self._t = t
+    #     self._u = u
+    #     self._hlm = d
+    #     pass
 
